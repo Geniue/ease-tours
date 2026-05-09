957e30eb42d6b1a1b96277b2d094ee8cc83ba4a8
Geniue <mostagenius@gmail.com>
2026-05-09 16:01:28 +0300

    fix: comment out GoogleReviewsSection import and usage in HomePage

--- frontend/src/app/[locale]/page.tsx
+++ frontend/src/app/[locale]/page.tsx
@@ -6,7 +6,7 @@ import TourCard from "@/components/TourCard";
 import BlogCard from "@/components/BlogCard";
 import ServiceCard from "@/components/ServiceCard";
 import WhyChooseUs from "@/components/WhyChooseUs";
-import GoogleReviewsSection from "@/components/GoogleReviewsSection";
+// import GoogleReviewsSection from "@/components/GoogleReviewsSection";
 import { getTrips, getBlogs, getServices } from "@/lib/api";
 import { Link } from "@/i18n/navigation";
 
@@ -127,7 +127,7 @@ export default async function HomePage() {
 
         <WhyChooseUs />
 
-        <GoogleReviewsSection />
+        {/* <GoogleReviewsSection /> */}
       </main>
     </>
   );
